0ca125c0d2ca74ce3ff224a685b6291ceef42e22
danielabramiani <daniel.abramiani4@gmail.com>
2024-07-27 17:00:07 +0400

    commit

--- explaining to my crew members/goga/file.py
+++ explaining to my crew members/goga/file.py
@@ -1,5 +1,35 @@
-#Convert a Number to a String!
+# # #Convert a Number to a String!
 
-def number_to_string(num):
-    return str(num)
+# # def number_to_string(num):
+# #     return str(num)
 
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+
+sum = 0
+for x in my_list:
+    sum += x
+print(sum)
+# even_numbers = []
+# for x in my_list:
+#     if x % 2 == 0:
+#         even_numbers.append(x)
+# print(even_numbers)
+
+# five_numbers = my_list[:5]
+# print(five_numbers)
+
+# last_five = my_list[-5:]
+# print(last_five)
+
+# three = []
+# for x in my_list:
+#     if x % 3 == 0:
+#         three.append(x)
+# print(three)
+
+# three_and_five = []
+# for x in my_list:
+#     if x % 3 == 0 and x % 5 == 0:
+#         three_and_five.append(x)
+# print(three_and_five)
